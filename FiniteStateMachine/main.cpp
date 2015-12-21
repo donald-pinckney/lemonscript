aff342906b6db3c1b0fb77e6833302f4069b8926
Donald Pinckney <donald_pinckney@icloud.com>
2015-12-20 20:32:08 -1000

    Initial Commit

---
+++ FiniteStateMachine/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FiniteStateMachine
+//
+//  Created by Donald Pinckney on 12/20/15.
+//  Copyright © 2015 Donald Pinckney. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
